fc263b6b7f559a4d1b8c32a125e76df23e270ae7
KristenSantos <Kristensxntos@gmail.com>
2023-05-02 14:53:57 -0400

    setting up

--- src/db/seeds/init.js
+++ src/db/seeds/init.js
@@ -1,16 +1,14 @@
-/* This seed file uses the existing `User` model to 
-create new users with hashed passwords. To seed your 
-database with other User data, update your model's `create` 
-function to accept other input fields first before updating
-this file.
-*/
-const User = require('../models/user');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.seed = async (knex) => {
-  await User.deleteAll();
-  await User.create('cool_cat', 'password1');
-  await User.create('l33t-guy', 'password1');
+const { userData, postData, comms } = require("../seedingData");
+
+exports.seed = async function (knex) {
+  await knex("users").del();
+  await knex("posts").del();
+  await knex("comments").del();
+  await knex("users").insert(userData);
+  await knex("posts").insert(postData);
+  await knex("comments").insert(comms);
 };
